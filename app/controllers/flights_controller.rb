af6f749ea73741b04cb63b702805d1a6de4fc77a
zdbrenda <zdbrenda@gmail.com>
2021-11-09 12:34:47 -0700

    Change on flights controller and flights index page to make the search form rendered again after search result returned

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,17 +1,22 @@
 class FlightsController < ApplicationController
 
     def index
-        
+        # @searched = false
         @search = params["search"]
+        # debugger
         if @search.present?
-            @dep_airport = @search["dep_airport_id"]
-            @arr_airport = @search["arr_airport_id"]
-            @start_datetime = @search["start_datetime"]
-            @day = @search["start_datetime(3i)"]
-            @month = @search["start_datetime(2i)"]
-            @year = @search["start_datetime(1i)"]
-            @start_datetime = (@year+"/"+@month+"/"+@day).to_date
-            @searched_flights = Flight.where(dep_airport_id:@dep_airport, arr_airport_id: @arr_airport, start_datetime: @start_datetime.all_day)
+            # @searched = true
+            dep_airport = @search["dep_airport_id"]
+            arr_airport = @search["arr_airport_id"]
+            num_of_passengers = @search["num_of_passengers"]
+            
+            day = @search["start_datetime(3i)"]
+            month = @search["start_datetime(2i)"]
+            year = @search["start_datetime(1i)"]
+            start_datetime = (year+"/"+month+"/"+day).to_date
+            @flights = Flight.where(dep_airport_id:dep_airport, arr_airport_id: arr_airport, start_datetime: start_datetime.all_day)
+            # @searched = false
+
         else
             @flights = Flight.limit(10).order("start_datetime")
         end
@@ -37,6 +42,6 @@ class FlightsController < ApplicationController
 
     private 
     def flight_params
-        params.require(:flight).permit(:dep_airport_id, :arr_airport_id, :start_datetime, :duration, :search)
+        params.require(:flight).permit(:dep_airport_id, :arr_airport_id, :start_datetime, :duration, :search, :num_of_passengers)
     end
 end
