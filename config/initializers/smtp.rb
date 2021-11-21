2ef16683663b4942e31e320cb9dce853617e2dde
zdbrenda <zdbrenda@gmail.com>
2021-11-21 16:55:01 -0700

    Add sendgrid api key config for sending emails in heroku deployment

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings={
     address:                'smtp.sendgrid.net',
     port:                   587,
-    domain:                 'gmail.com',
-    user_name:              ENV['SENDGRID_USERNAME'],
-    password:               ENV['SENDGRID_PASSWORD'],
-    authentication: :login,
-    enable_starttls_auto:   true
+    domain:                 'sendgrid-rigid-59209.herokuapp.com',
+    user_name:              'apikey',
+    password:               ENV['SENDGRID_API_KEY'],
+    authentication: :plain,
+    
 }
 ActionMailer::Base.delivery_method = :smtp
