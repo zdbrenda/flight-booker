a336036e64effba34d6f311fb0967c5a2da6c4f2
zdbrenda <zdbrenda@gmail.com>
2021-11-21 12:34:48 -0700

    Add changes for sending emails from sendgrid for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,15 +63,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "flight_booker_production"
 
   config.action_mailer.perform_caching = true
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "example.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["GMAIL_USERNAME"],
+  #   password: ENV["GMAIL_PASSWORD"]
 
+  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    host: "https://www.gmail.com"
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
---
+++ config/initializers/smtp.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.smtp_settings={
+    address:                'smtp.sendgrid.net',
+    port:                   587,
+    domain:                 'gmail.com',
+    user_name:              ENV['SENDGRID_USERNAME'],
+    password:               ENV['SENDGRID_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto:   true
+}
+ActionMailer::Base.delivery_method = :smtp
