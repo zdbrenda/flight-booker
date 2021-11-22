2d4e2999d27a77429a8150a04062a7c324c39bb5
zdbrenda <zdbrenda@gmail.com>
2021-11-22 14:18:50 -0700

    Add config changes for sending emails in heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-require Rails.root.join("config/initializers/smtp")
+require Rails.root.join("config/smtp")
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -78,9 +78,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {
-    host: "sendgrid-rigid-59209.herokuapp.com"
-  }
+  config.action_mailer.smtp_settings = SMTP_SETTINGS
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
