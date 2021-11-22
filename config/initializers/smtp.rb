5dae7c0dcd52a90a2308cb705651825752bb6400
zdbrenda <zdbrenda@gmail.com>
2021-11-21 17:11:23 -0700

    Change domain config for sending emails in heroku deployment

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings={
     address:                'smtp.sendgrid.net',
     port:                   587,
-    domain:                 'sendgrid-rigid-59209.herokuapp.com',
+    domain:                 ENV["SENDGRID_EMAIL_DOMAIN"],
     user_name:              'apikey',
     password:               ENV['SENDGRID_API_KEY'],
     authentication: :plain,
