259d1cd78e9ada8ba30df64675e07b469cd73443
zdbrenda <zdbrenda@gmail.com>
2021-11-22 19:24:26 -0700

    Add passenger name in PassengerMailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -11,14 +11,14 @@ class PassengerMailer < ApplicationMailer
         
         @passengers.each do |passenger|
             
-            # mail(to: passenger.email, subject: "Confirmation of Flight Booking")g
-            PassengerMailer.new_passenger(passenger.email, booking).deliver_now
+            PassengerMailer.new_passenger(passenger.email, passenger.name, booking).deliver_now
         end
     end
 
-    def new_passenger(email, booking)
+    def new_passenger(email, name, booking)
         @booking = booking
         @flight = @booking.flight
+        @name = name
         @dep_airport = @flight.dep_airport.code
         @arr_airport = @flight.arr_airport.code
         @start_datetime = @flight.start_datetime
