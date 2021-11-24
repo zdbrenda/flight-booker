1c3eded499ce3f49101c393103878546ca44e17a
zdbrenda <zdbrenda@gmail.com>
2021-11-24 15:35:47 -0700

    Add changes to flights table

--- db/migrate/20211102204305_create_flight_flights.rb
+++ db/migrate/20211102204305_create_flight_flights.rb
@@ -1,15 +1,15 @@
 class CreateFlightFlights < ActiveRecord::Migration[6.1]
   def change
     create_table :flights do |t|
-      t.references :dep_airport, null: false, index:true
-      t.references :arr_airport, null: false, index:true
+      t.references :dep_airport, null: false, foreign_key: { to_table: :airports }
+      t.references :arr_airport, null: false, foreign_key: { to_table: :airports }
       t.datetime :start_datetime
       t.integer :duration
 
       t.timestamps
     end
 
-    add_foreign_key :flights, :airports, column: dep_airport_id
-    add_foreign_key :flights, :airports, column: arr_airport_id
+    # add_foreign_key :flights, :airports, column: dep_airport_id
+    # add_foreign_key :flights, :airports, column: arr_airport_id
   end
 end
